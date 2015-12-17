d856e3df23119482ddb5ebbe144c9adadc7eb335
MrCubanfrog <ilmosal@gmail.com>
2015-12-17 15:26:33 +0200

    Create main.f95

---
+++ main.f95
@@ -0,0 +1,29 @@
+program main
+  use object_space_mod
+  use verlet_integrator_mod
+  use inputReader_mod
+  implicit none
+
+  character(100) :: filename, outputName
+  integer :: numberofObjects, steps, everyNth, error
+  real :: timescale
+  type(object_space), allocatable :: list(:)
+
+  filename = "../run/input.dat"
+
+  error = readStats(filename, outputName, numberofObjects, timescale, steps, everyNth)
+
+  if (error == 0) then
+    stop
+  end if
+
+  error = readObjects(filename, list, numberofObjects)
+
+  if (error == 0) then
+    stop
+  end if
+
+  call run_verlet_integration(list, numberofObjects, timescale, steps, everyNth, outputName)
+
+  print *, "Run succesful."
+end program

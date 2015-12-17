396fcce2fe675ebc51678160ad4e97a8a339c602
MrCubanfrog <ilmosal@gmail.com>
2015-12-18 01:35:25 +0200

    Update main.f95

--- main.f95
+++ main.f95
@@ -1,11 +1,12 @@
 program main
   use object_space_mod
   use verlet_integrator_mod
+  use runge_kutta_mod
   use inputReader_mod
   implicit none
 
   character(100) :: filename, outputName
-  integer :: numberofObjects, steps, everyNth, error
+  integer :: numberofObjects, steps, everyNth, error, sim_type
   real :: timescale
   type(object_space), allocatable :: list(:)
 
@@ -23,7 +24,10 @@ program main
     stop
   end if
 
-  call run_verlet_integration(list, numberofObjects, timescale, steps, everyNth, outputName)
-
+  if (sim_type == 1) then 
+    call run_verlet_integration(list, numberofObjects, timescale, steps, everyNth, outputName)
+  else if (sim_type == 2) then
+    call run_runge_kutta(list, timescale, numberofObjects, steps, outputName, everyNth)  
+  end if
   print *, "Run succesful."
 end program
